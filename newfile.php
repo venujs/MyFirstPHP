8c47608f795a9c1ba6456574c72314c800c2bb67
venujs <venujs@gmail.com>
2016-11-21 16:48:57 +0530

    test

--- newfile.php
+++ newfile.php
@@ -7,6 +7,6 @@
             echo "<p>Hello World!</p>";
         ?>
         <a href="login.php"> Click here to login </a>
-        <a href="register.php"> Click here to register </a> 
+        <a href="RegnPage.php"> Click here to register </a> 
     </body>
 </html> 
